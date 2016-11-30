7573e129f452b877f4f9f97bde343749d979634f
mauriziodm <mauriziodm@levantesw.it>
2016-11-30 16:40:31 +0100

    no message

--- Sources/DJSON.Attributes.pas
+++ Sources/DJSON.Attributes.pas
@@ -11,17 +11,23 @@
 {                                                                           }
 {***************************************************************************}
 {                                                                           }
-{  Licensed under the Apache License, Version 2.0 (the "License");          }
+{  This file is part of DJSON (Delphi JSON library).                        }
+{                                                                           }
+{  Licensed under the GNU Lesser General Public License, Version 3;         }
 {  you may not use this file except in compliance with the License.         }
-{  You may obtain a copy of the License at                                  }
 {                                                                           }
-{      http://www.apache.org/licenses/LICENSE-2.0                           }
+{  DJSON is free software: you can redistribute it and/or modify            }
+{  it under the terms of the GNU Lesser General Public License as published }
+{  by the Free Software Foundation, either version 3 of the License, or     }
+{  (at your option) any later version.                                      }
 {                                                                           }
-{  Unless required by applicable law or agreed to in writing, software      }
-{  distributed under the License is distributed on an "AS IS" BASIS,        }
-{  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. }
-{  See the License for the specific language governing permissions and      }
-{  limitations under the License.                                           }
+{  DJSON is distributed in the hope that it will be useful,                 }
+{  but WITHOUT ANY WARRANTY; without even the implied warranty of           }
+{  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            }
+{  GNU Lesser General Public License for more details.                      }
+{                                                                           }
+{  You should have received a copy of the GNU Lesser General Public License }
+{  along with DJSON.  If not, see <http://www.gnu.org/licenses/>.           }
 {                                                                           }
 {***************************************************************************}
 {                                                                           }
@@ -29,6 +35,8 @@
 {  Delphi MVC Framework project by Daniele Teti and the DMVCFramework Team. }
 {                                                                           }
 {***************************************************************************}
+
+
 
 
 
