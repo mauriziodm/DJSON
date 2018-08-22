c0130f871e4856f2f9e6c2504c15e386cb385eb4
mauriziodm <mauriziodm@levantesw.it>
2018-08-22 21:50:36 +0200

    no message

--- Sources/DJSON.TypeInfoCache.pas
+++ Sources/DJSON.TypeInfoCache.pas
@@ -119,7 +119,8 @@ begin
       Result.DuckType := TdjDuckType.dtStreamable
     else
       Result.DuckType := TdjDuckType.dtNone;
-    FInternatContainer.Add(AObj.ClassName, Result);
+    if FEnabled then
+      FInternatContainer.Add(AObj.ClassName, Result);
   end;
 end;
 
